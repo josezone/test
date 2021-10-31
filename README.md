ff1a105dabd000448da252efe5a25a64b5e9febf
maddev.jose <joset@yeswearemad.com>
2021-10-31 15:21:26 +0530

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goodbits
+nodeapp contains node application
+reactapp contains react application
+
+env variable for node app is in https://github.com/josezone/test/blob/master/nodeapp/src/config/envs.ts
+db used is mysql
--- reactapp/src/ui/forms/field-config.ts
+++ reactapp/src/ui/forms/field-config.ts
@@ -1,20 +1,10 @@
 import { ElementType } from "react";
-import { CheckBoxFields } from "./fields/checkboxField";
 import InputFields from "./fields/InputField";
-import MaskedInputFields from "./fields/maskedInputField";
-import { RadioFields } from "./fields/radioField";
 import { SecondaryButton } from "./fields/SecondaryButton";
-import SelectFields from "./fields/selectField";
 import { SubmitButton } from "./fields/SubmitButton";
-import TextFields from "./fields/textField";
 
 export const FieldConfig: {[key: string]: ElementType} = {
     input: InputFields,
     submitButton: SubmitButton,
-    button: SecondaryButton,
-    select: SelectFields,
-    checkbox: CheckBoxFields,
-    radio: RadioFields,
-    text: TextFields,
-    maskedInput: MaskedInputFields
+    button: SecondaryButton
 }
